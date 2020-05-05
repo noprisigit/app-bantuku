3f20c7aecc07927d382d7be5fd4434122cac9a63
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-05-05 12:01:45 +0700

    create method index in category

--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Category extends CI_Controller {
     public function index()
     {
-        
+        $data['main_title'] = "Category";
+        $data['title'] = "Category";
+
+        $this->load->view('template/header', $data);
+        $this->load->view('category/index');
+        $this->load->view('template/footer');
     }
 }
