bbcba10e27701cc66744c81564fcb537fd431645
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-05-14 16:10:09 +0700

    create api order controller

---
+++ application/controllers/api/Order.php
@@ -0,0 +1,11 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Order extends REST_Controller
+{
+   
+}
