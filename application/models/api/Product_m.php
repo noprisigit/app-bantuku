48fb26d84b7f3dafc79155315758af685c626278
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-05-21 01:23:32 +0700

    update api/Product_m getProductsByCategory

--- application/models/api/Product_m.php
+++ application/models/api/Product_m.php
@@ -4,9 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Product_m extends CI_Model {
    public function getProductsByCategory($categoryID)
    {
-      $this->db->where('CategoryID', $categoryID);
-      $this->db->where('ProductStatus', 1);
-      return $this->db->get('products')->result();
+      $query = $this->db->query('SELECT categories.CategoryID, categories.CategoryName, products.PartnerID, products.ProductUniqueID, products.ProductName, products.ProductPrice, products.ProductStock, products.ProductWeight, products.ProductDesc, products.ProductImage, products.ProductThumbnail, products.ProductStatus, products.ProductPromo, products.ProductStatusPromo, products.ProductPromoDate, ROUND(AVG(orders.Rating), 0) AS Rating FROM products INNER JOIN categories ON products.CategoryID = categories.CategoryID INNER JOIN orders ON products.ProductUniqueID = orders.ProductUniqueID WHERE categories.CategoryID = '. $categoryID .' AND products.ProductStatus = 1 AND orders.OrderStatus > 1 GROUP BY orders.ProductUniqueID');
+      return $query->result();
    }
 
    public function getProductsByPromoToday()
