11b8efb97ab0afcd35f51a1e0d1c527c142e204f
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-05-21 03:06:20 +0700

    update getProductsByPromoToday method model

--- application/models/api/Product_m.php
+++ application/models/api/Product_m.php
@@ -10,7 +10,7 @@ class Product_m extends CI_Model {
 
    public function getProductsByPromoToday()
    {
-      $query = $this->db->query('SELECT * FROM `products` WHERE ProductStatus = 1 and ProductStatusPromo = 1 and ProductPromoDate = CURRENT_DATE()');
+      $query = $this->db->query('SELECT categories.CategoryID, categories.CategoryName, products.PartnerID, products.ProductUniqueID, products.ProductName, products.ProductPrice, products.ProductStock, products.ProductWeight, products.ProductDesc, products.ProductImage, products.ProductThumbnail, products.ProductStatus, products.ProductPromo, products.ProductStatusPromo, products.ProductPromoDate, IFNULL(ROUND(AVG(orders.Rating), 0), 0) AS Rating FROM products LEFT JOIN categories ON products.CategoryID = categories.CategoryID LEFT JOIN orders ON products.ProductUniqueID = orders.ProductUniqueID WHERE products.ProductStatus = 1 AND products.ProductStatusPromo = 1 AND products.ProductPromoDate = CURRENT_DATE() GROUP BY products.ProductUniqueID');
       return $query->result();
    }
 
