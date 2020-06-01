77d24cea359034d9ddf0fb326931c38e382e70dc
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-06-01 21:44:59 +0700

    configure sendEmail for customer register

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -70,6 +70,7 @@ class Auth extends REST_Controller
         $password = $this->post('password');
         $confirm_password = $this->post('confirm_password');
         $address = $this->post('address');
+        $gender = $this->post('gender');
 
         if ($full_name === null || $email === null || $phone === null || $password === null || $confirm_password === null || $address === null) {
             $this->response([
@@ -91,6 +92,7 @@ class Auth extends REST_Controller
                 $data = [
                     'CustomerUniqueID'          => $uniqueID,
                     'CustomerName'              => $full_name,
+                    'CustomerGender'            => $gender,
                     'CustomerEmail'             => $email,
                     'CustomerPhone'             => $phone,
                     'CustomerPassword'          => password_hash($password, PASSWORD_DEFAULT),
@@ -100,15 +102,7 @@ class Auth extends REST_Controller
                     'CustomerLoginToken'        => $token
                 ];
     
-                // $emailToken = base64_encode(random_bytes(64));
-
-                // $access = [
-                //     'CustomerUniqueID'      => $uniqueID,
-                //     'CustomerEmailToken'    => $emailToken
-                // ];
-                // $this->auth->registration_token($access);
-
-                // $this->_sendEmail($generateCode, 'verify');
+                $this->_sendEmail($generateCode, 'verify');
     
                 if ($this->auth->registration($data) > 0) {
                     $this->response([
@@ -137,33 +131,126 @@ class Auth extends REST_Controller
     }
 
     private function _sendEmail($value, $type) {
-        $config = [
+        $config= [
             'protocol'      => 'smtp',
             'smtp_host'     => 'ssl://smtp.googlemail.com',
-            'smtp_user'     => 'neatinw@gmail.com',
-            'smtp_pass'     => '19111998',
+            'smtp_user'     => 'bantuku2020@gmail.com',
+            'smtp_pass'     => '.BantukuBabelProv20',
             'smtp_port'     => 465,
+            'smtp_timeout'  => '5',
             'mailtype'      => 'html',
             'charset'       => 'utf-8',
             'newline'       => "\r\n"
         ];
 
         $this->load->library('email', $config);
-        $this->email->from('neatinw@gmail.com', 'bantuku');
-        $this->email->to($this->post('email'));
 
+        $this->email->from('bantuku2020@gmail.com', 'Bantuku Support');
+        $this->email->to($this->post('email'));
+        $linkImage = "http://bantuku2020.babelprov.go.id/assets/dist/img/tick.png";
+        
         if ($type == 'verify') {
-            $this->email->subject('Account Verication Code');
-            $this->email->message('Silahkan masukkan kode berikut ini: ' . $value);
+            $this->email->subject('Konfirmasi Pendaftaran');
+            $this->email->message('
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                </head>
+                <body style="margin: 0; padding: 0; font-family: Cambria, Cochin, Georgia, Times, Times New Roman, serif;">
+                <table align="center" cellpading="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+                    <tr>
+                        <td align="center" style="background-image: linear-gradient(to right bottom, #00C6FF, #0072FF); padding: 30px 0 30px 0;">
+                            <img src="'.$linkImage.'" alt="CheckList Icon" width="128">
+                            <h1 style="color: #FFFFFF; margin-top: 1px; margin-bottom: 5px">Terima Kasih</h1>
+                            <h4 style="color: #FFFFFF; margin: 10px 0 10px 0;">Anda Sudah Terdaftar pada Aplikasi Bantuku.</h4>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#FFFFFF" style="padding: 10px 20px 40px 20px;">
+                            <table cellpadding="0" cellspacing="0" width="100%">
+                                <tr>
+                                    <td align="center" bgcolor="#b4b8bf" colspan="2" style="padding: 20px 0 20px 0; font-size: 20px; color: #FFFFFF;">Detail Pelanggan</td>
+                                </tr>
+                                <tr>
+                                    <td  style="padding: 5px 0 5px 5px;">Nama Customer</td>
+                                    <td>: '.$this->post("full_name").'</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 5px 0 5px 5px;">Jenis Kelamin</td>
+                                    <td>: '.$this->post("gender").'</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 5px 0 5px 5px;">Email</td>
+                                    <td>: '.$this->post("email").'</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 5px 0 5px 5px;">No. Telp</td>
+                                    <td>: '.$this->post("phone").'</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 5px 0 5px 5px;">Alamat</td>
+                                    <td>: '.$this->post("address").'</td>
+                                </tr>
+                                <tr>
+                                    <td align="center" colspan="2" style="padding: 20px 0 5px 5px;">Silahkan Masukkan Kode Verifikasi Berikut Ini untuk Memverifikasi Akun Anda</td>
+                                </tr>
+                                <tr>
+                                    <td align="center" colspan="2" style="padding: 10px 0 5px 5px;">
+                                        <span style="padding: 5px;background-color: #0072FF; font-size: 25px; color: #FFFFFF;">'.$value.'</span>  
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <!-- <tr>
+                        <td bgcolor="#0072FF">Row 3</td>
+                    </tr> -->
+                </table>
+                </body>
+                </html>
+            ');
         }
 
         if ($this->email->send()) {
+            // echo "Email berhasil dikirim ke " . $this->post('email');
             return true;
         } else {
             echo $this->email->print_debugger();
             die;
         }
     }
+
+    // private function _sendEmail($value, $type) {
+    //     $config = [
+    //         'protocol'      => 'smtp',
+    //         'smtp_host'     => 'ssl://smtp.googlemail.com',
+    //         'smtp_user'     => 'neatinw@gmail.com',
+    //         'smtp_pass'     => '19111998',
+    //         'smtp_port'     => 465,
+    //         'mailtype'      => 'html',
+    //         'charset'       => 'utf-8',
+    //         'newline'       => "\r\n"
+    //     ];
+
+    //     $this->load->library('email', $config);
+    //     $this->email->from('neatinw@gmail.com', 'bantuku');
+    //     $this->email->to($this->post('email'));
+
+    //     if ($type == 'verify') {
+    //         $this->email->subject('Account Verication Code');
+    //         $this->email->message('Silahkan masukkan kode berikut ini: ' . $value);
+    //     }
+
+    //     if ($this->email->send()) {
+    //         return true;
+    //     } else {
+    //         echo $this->email->print_debugger();
+    //         die;
+    //     }
+    // }
     
     public function verifiedEmail_post()
     {
